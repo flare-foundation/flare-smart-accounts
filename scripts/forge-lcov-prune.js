aa75b1a3bac48fb0f5ad9ef6e32df8437ff7d57d
Timotej Vesel <timotej.vesel@gmail.com>
2025-11-10 13:26:37 +0100

    exclude test utils from coverage

--- scripts/forge-lcov-prune.js
+++ scripts/forge-lcov-prune.js
@@ -1,5 +1,5 @@
 const fs = require("node:fs");
-let forgeLcovFile = fs.readFileSync('lcov.info','utf8');
+let forgeLcovFile = fs.readFileSync('lcov.info', 'utf8');
 let prunedForgeLcovPath = 'lcov.info.pruned';
 if (fs.existsSync(prunedForgeLcovPath)) {
   fs.unlinkSync(prunedForgeLcovPath);
@@ -7,11 +7,14 @@ if (fs.existsSync(prunedForgeLcovPath)) {
 
 let del = false;
 for (let line of forgeLcovFile.split('\n')) {
-  if (line.includes('test/mock/') || line.includes('contracts/mock')) {
+  if (
+    line.includes('test/mock/') ||
+    line.includes('contracts/mock') ||
+    line.includes('test/utils/')) {
     del = true;
   } else if (line.includes('end_of_record') && del) {
     del = false;
-  } else if (!del){
+  } else if (!del) {
     fs.appendFileSync(prunedForgeLcovPath, line + '\n');
   }
 }
